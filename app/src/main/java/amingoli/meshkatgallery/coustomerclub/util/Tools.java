5f3c52540ffd6414816442116eab05cb24d4ddcd
amingoli78 <amin.goli78@gmail.com>
2020-07-02 20:19:16 +0430

    copy to clip board

--- app/src/main/java/amingoli/meshkatgallery/coustomerclub/util/Tools.java
+++ app/src/main/java/amingoli/meshkatgallery/coustomerclub/util/Tools.java
@@ -1,5 +1,11 @@
 package amingoli.meshkatgallery.coustomerclub.util;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.widget.Toast;
+
+import amingoli.meshkatgallery.coustomerclub.R;
 import saman.zamani.persiandate.PersianDate;
 import saman.zamani.persiandate.PersianDateFormat;
 
@@ -7,5 +13,11 @@ public class Tools {
     public static String getFormattedDateSimple(Long dateTime) {
         PersianDateFormat pdformater = new PersianDateFormat("l j F Y");
         return FaNum.convert(pdformater.format(new PersianDate(dateTime)));
+    }
+    public static void copyToClipboard(Context context, String data) {
+        ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("clipboard", data);
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(context, R.string.msg_copied_clipboard, Toast.LENGTH_SHORT).show();
     }
 }
