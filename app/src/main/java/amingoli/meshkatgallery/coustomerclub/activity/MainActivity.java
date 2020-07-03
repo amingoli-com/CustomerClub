6d68bb87d73201b1b306b89ab6f1a9acce9e5aac
amingoli78 <amin.goli78@gmail.com>
2020-07-03 11:07:04 +0430

    add permission CAMERA

--- app/src/main/java/amingoli/meshkatgallery/coustomerclub/activity/MainActivity.java
+++ app/src/main/java/amingoli/meshkatgallery/coustomerclub/activity/MainActivity.java
@@ -1,15 +1,24 @@
 package amingoli.meshkatgallery.coustomerclub.activity;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import amingoli.meshkatgallery.coustomerclub.R;
 
@@ -27,7 +36,9 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn_scan).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, ScanActivity.class));
+                if (permission()){
+                    startScanActivity();
+                }
             }
         });
     }
@@ -54,5 +65,107 @@ public class MainActivity extends AppCompatActivity {
             winParams.flags &= ~bits;
         }
         win.setAttributes(winParams);
+    }
+
+    /**
+     * check Permission for SMS
+     * */
+    private boolean dialogIsRun = false;
+    private static final int CAMERA_PERMISSION_CONSTANT = 100;
+    private static final int REQUEST_PERMISSION_SETTING = 101;
+    private boolean sentToSettings = false;
+    private boolean permission(){
+        final String[] a = {Manifest.permission.CAMERA};
+
+        SharedPreferences permissionStatus = getSharedPreferences("permissionStatus", MODE_PRIVATE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                //Show Information about why you need the permission
+                dialogIsRun = true;
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(R.string.desc_need_permission);
+                builder.setPositiveButton(R.string.ok_permission, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialogIsRun = false;
+                        dialog.cancel();
+                        ActivityCompat.requestPermissions(MainActivity.this, a, CAMERA_PERMISSION_CONSTANT);
+                    }
+                });
+                builder.setNegativeButton(R.string.exit_app, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialogIsRun = false;
+                        finish();
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            } else if (permissionStatus.getBoolean(Manifest.permission.CAMERA,false)) {
+                //Previously Permission Request was cancelled with 'Dont Ask Again',
+                // Redirect to Settings after showing Information about why you need the permission
+                dialogIsRun = true;
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(R.string.desc_need_permission);
+                builder.setPositiveButton(R.string.ok_permission, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialogIsRun = false;
+                        dialog.cancel();
+                        sentToSettings = true;
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package", getPackageName(), null);
+                        intent.setData(uri);
+                        startActivityForResult(intent, REQUEST_PERMISSION_SETTING);
+                    }
+                });
+                builder.setNegativeButton(R.string.exit_app, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialogIsRun = false;
+                        finish();
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            } else {
+                //just request the permission
+                ActivityCompat.requestPermissions(this, a, CAMERA_PERMISSION_CONSTANT);
+            }
+
+            SharedPreferences.Editor editor = permissionStatus.edit();
+            editor.putBoolean(Manifest.permission.CAMERA,true);
+            editor.apply();
+            return false;
+
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case CAMERA_PERMISSION_CONSTANT: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startScanActivity();
+                    // call your method
+                } else {
+                    permission();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void startScanActivity(){
+        startActivity(new Intent(MainActivity.this, ScanActivity.class));
     }
 }
